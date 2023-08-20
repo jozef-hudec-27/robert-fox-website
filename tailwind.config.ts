c86dd565e521d0bc2d12530ef1a07a0c45e169e8
Jozef Hudec <jojohudec27@gmail.com>
2023-08-20 13:05:27 +0200

    Set up typography and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,33 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        lato: ['var(--font-lato)'],
+        josefinSans: ['var(--font-josefin-sans)'],
       },
+      colors: {
+        black: {
+          100: '#3A3134',
+          75: '#6B6567',
+          50: '#9D989A',
+          25: '#CECBCC',
+          10: '#EBEAEA',
+          5: '#F5F4F5',
+        },
+        primary: '#FF4C80',
+        secondary: '#FF6840',
+        error: '#E81C0E',
+      },
+    },
+    fontSize: {
+      xs: '0.5rem',
+      sm: '0.707rem',
+      base: '1rem',
+      lg: '1.414rem',
+      xl: '1.999rem',
+      '2xl': '2.827rem',
+      '3xl': '3.998rem',
+      '4xl': '5.653rem',
     },
   },
   plugins: [],
