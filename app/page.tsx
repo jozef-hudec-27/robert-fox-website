fb21531ac4e62a72f6ccf24f2808bc3fd764f898
Jozef Hudec <jojohudec27@gmail.com>
2023-08-20 20:41:30 +0200

    Make hero section responsive

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,17 @@ import { BsChevronDoubleDown } from 'react-icons/bs'
 function IndexPage() {
   return (
     <div>
-      <section id="hero" className="flex flex-col items-center gap-[32px] w-3/4 mx-auto mt-[48px]">
+      <section id="hero" className="flex flex-col items-center gap-[12px] md:gap-[24px] lg:gap-[32px] w-11/12 lg:w-3/4 mx-auto mt-[48px]">
         <h3 className="font-light">Hey there, I'm Robert</h3>
 
         <h1 className="gradient-text gradient-50 text-center">Freelance UI Designer & Web Developer</h1>
 
-        <p className="w-1/2 text-center">
+        <p className="w-full lg:w-1/2 text-center">
           I help business grow by crafting amazing web experiences. If you’re looking for a designer and developer that
           likes to get stuff done, let’s talk.
         </p>
 
-        <button className="btn primary mt-[64px]">
+        <button className="btn primary mt-[32px] md:mt-[48px] lg:mt-[64px]">
           <span>See my work</span>
           <BsChevronDoubleDown />
         </button>
